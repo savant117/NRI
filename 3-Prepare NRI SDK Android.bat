43b29ff39bd204f81d12ab3963b852ac6e6872b1
Chris Giles <cgiles17@gmail.com>
2023-02-05 16:51:36 -0800

    Fixed android SDK script

--- 3-Prepare NRI SDK Android.bat
+++ 3-Prepare NRI SDK Android.bat
@@ -5,15 +5,19 @@ set NRI_DIR=.
 rd /q /s "_NRI_SDK"
 
 mkdir "_NRI_SDK\Include\Extensions"
-mkdir "_NRI_SDK\Lib\Debug"
-mkdir "_NRI_SDK\Lib\Release"
+mkdir "_NRI_SDK\Lib\Android\x86_64\Debug"
+mkdir "_NRI_SDK\Lib\Android\x86_64\Release"
+mkdir "_NRI_SDK\Lib\Android\arm64-v8a\Debug"
+mkdir "_NRI_SDK\Lib\Android\arm64-v8a\Release"
 
 cd "_NRI_SDK"
 
 copy "..\%NRI_DIR%\Include\*" "Include"
 copy "..\%NRI_DIR%\Include\Extensions\*" "Include\Extensions"
-copy "..\_Build\Debug\libNRI.a" "Lib\Debug"
-copy "..\_Build\Release\libNRI.a" "Lib\Release"
+copy "..\_Build\Android\x86_64\Debug\libNRI.so" "Lib\Android\x86_64\Debug"
+copy "..\_Build\Android\x86_64\Release\libNRI.so" "Lib\Android\x86_64\Release"
+copy "..\_Build\Android\arm64-v8a\Debug\libNRI.so" "Lib\Android\arm64-v8a\Debug"
+copy "..\_Build\Android\arm64-v8a\Release\libNRI.so" "Lib\Android\arm64-v8a\Release"
 copy "..\%NRI_DIR%\LICENSE.txt" "."
 
 cd ..
